3c1f8270f37ab0e090b5cbb6781e9d2bfb5cc2bd
Adolf David <143256974+adolfzcoder@users.noreply.github.com>
2024-11-18 18:17:24 +0200

    changes

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-import tabula       file = open("data\grade11results-2024.csv", "r") print(file.read())
+filePath = "data\grade11results-2024.csv"       
+with open(filePath, 'r') as file:
+    lines = file.readlines()[4:]  
+    for line in lines:
+        print(f"Candidate Number: {line[1]}")
