639caa3aa113eff88012deadaafd6e130f9ed8e0
Adolf David <143256974+adolfzcoder@users.noreply.github.com>
2024-11-21 22:17:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ filePath = "data\grade11results-2024.csv"
 with open(filePath, 'r') as file:
     lines = file.readlines()[4:]  
     for line in lines:
+        
         print(f"Candidate Number: {line[1]}")
